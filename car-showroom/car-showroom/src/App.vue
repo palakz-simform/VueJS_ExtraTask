ee93436452e93fcc492b6acca1c93e78d2cc1843
Palak Zalavadia <palak.z@simformsolutions.com>
2023-03-06 18:00:52 +0530

    UI of Car Gallery

--- car-showroom/car-showroom/src/App.vue
+++ car-showroom/car-showroom/src/App.vue
@@ -1,11 +1,118 @@
-<script setup>
-
-</script>
-
 <template>
- 
+<div class="title">
+    <h1>Car Gallery</h1>
+</div>
+
+<div class="car-card">
+    <div v-for="item in cars_info" :key="item.id">
+        <gallery_card :data="item" />
+    </div>
+
+</div>
 </template>
 
-<style >
+<script>
+import gallery_card from "./components/gallery_card.vue"
 
+export default {
+    name: "App",
+    components: {
+        gallery_card,
+
+    },
+    data() {
+        return {
+            cars_info: [{
+                    id: 1,
+                    name: "Toyota",
+                    image: "",
+                    desc: "",
+                    price: ""
+                },
+                {
+                    id: 2,
+                    name: "Toyota",
+                    image: "",
+                    desc: "",
+                    price: ""
+                },
+                {
+                    id: 3,
+                    name: "Toyota",
+                    image: "",
+                    desc: "",
+                    price: ""
+                },
+                {
+                    id: 4,
+                    name: "Toyota",
+                    image: "",
+                    desc: "",
+                    price: ""
+                },
+                {
+                    id: 5,
+                    name: "Toyota",
+                    image: "",
+                    desc: "",
+                    price: ""
+                },
+                {
+                    id: 6,
+                    name: "Toyota",
+                    image: "",
+                    desc: "",
+                    price: ""
+                },
+                {
+                    id: 7,
+                    name: "Toyota",
+                    image: "",
+                    desc: "",
+                    price: ""
+                },
+                {
+                    id: 8,
+                    name: "Toyota",
+                    image: "",
+                    desc: "",
+                    price: ""
+                },
+                {
+                    id: 9,
+                    name: "Toyota",
+                    image: "",
+                    desc: "",
+                    price: ""
+                },
+                {
+                    id: 10,
+                    name: "Toyota",
+                    image: "",
+                    desc: "",
+                    price: ""
+                },
+
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+.car-card {
+    display: flex;
+    ;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+.title {
+    text-align: center;
+}
+
+.title h1 {
+    color: brown;
+    text-decoration: underline;
+}
 </style>
